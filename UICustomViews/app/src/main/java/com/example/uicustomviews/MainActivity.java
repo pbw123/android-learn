8e44bbe37a6abb337d506dc1f2f533d4fc2bd108
pbw123 <2635225112@qq.com>
2019-09-04 15:26:33 +0800

    3.4.2 创建自定义系统控件

--- UICustomViews/app/src/main/java/com/example/uicustomviews/MainActivity.java
+++ UICustomViews/app/src/main/java/com/example/uicustomviews/MainActivity.java
@@ -11,8 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        隐藏系统默认标题栏
         ActionBar actionBar = getSupportActionBar();
-        if (actionBar!=null)
+        if (actionBar!=null){
             actionBar.hide();
+        }
     }
 }
