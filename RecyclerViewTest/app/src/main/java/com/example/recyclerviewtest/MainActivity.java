418fe26101f15832e2a8969c04275dfc0fdec07e
pbw123 <2635225112@qq.com>
2019-09-04 21:35:07 +0800

    RecyclerView实现瀑布流布局

--- RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
+++ RecyclerViewTest/app/src/main/java/com/example/recyclerviewtest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.recyclerviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.os.Bundle;
 import android.view.View;
@@ -10,32 +11,44 @@ import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private List<Fruit> fruitList = new ArrayList<>();
 
+    private String getRandomLengthName(String name)
+    {
+        Random random = new Random();
+        int length = random.nextInt(20) + 1;
+        StringBuilder builder = new StringBuilder();
+        for (int i=0;i<length;i++)
+        {
+            builder.append(name);
+        }
+        return builder.toString();
+    }
     private void initFruits() {
         for (int i = 0; i < 2; i++) {
-            Fruit apple = new Fruit(("Apple"), R.drawable.apple_pic);
+            Fruit apple = new Fruit(getRandomLengthName("Apple"), R.drawable.apple_pic);
             fruitList.add(apple);
-            Fruit banana = new Fruit(("Banana"), R.drawable.banana_pic);
+            Fruit banana = new Fruit(getRandomLengthName("Banana"), R.drawable.banana_pic);
             fruitList.add(banana);
-            Fruit orange = new Fruit(("Orange"), R.drawable.orange_pic);
+            Fruit orange = new Fruit(getRandomLengthName("Orange"), R.drawable.orange_pic);
             fruitList.add(orange);
-            Fruit watermelon = new Fruit(("Watermelon"), R.drawable.watermelon_pic);
+            Fruit watermelon = new Fruit(getRandomLengthName("Watermelon"), R.drawable.watermelon_pic);
             fruitList.add(watermelon);
-            Fruit pear = new Fruit(("Pear"), R.drawable.pear_pic);
+            Fruit pear = new Fruit(getRandomLengthName("Pear"), R.drawable.pear_pic);
             fruitList.add(pear);
-            Fruit grape = new Fruit(("Grape"), R.drawable.grape_pic);
+            Fruit grape = new Fruit(getRandomLengthName("Grape"), R.drawable.grape_pic);
             fruitList.add(grape);
-            Fruit pineapple = new Fruit(("Pineapple"), R.drawable.pineapple_pic);
+            Fruit pineapple = new Fruit(getRandomLengthName("Pineapple"), R.drawable.pineapple_pic);
             fruitList.add(pineapple);
-            Fruit strawberry = new Fruit(("Strawberry"), R.drawable.strawberry_pic);
+            Fruit strawberry = new Fruit(getRandomLengthName("Strawberry"), R.drawable.strawberry_pic);
             fruitList.add(strawberry);
-            Fruit cherry = new Fruit(("Cherry"), R.drawable.cherry_pic);
+            Fruit cherry = new Fruit(getRandomLengthName("Cherry"), R.drawable.cherry_pic);
             fruitList.add(cherry);
-            Fruit mango = new Fruit(("Mango"), R.drawable.mango_pic);
+            Fruit mango = new Fruit(getRandomLengthName("Mango"), R.drawable.mango_pic);
             fruitList.add(mango);
         }
     }
@@ -49,9 +62,13 @@ public class MainActivity extends AppCompatActivity {
 //        先获得到RecyclerView的实例
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
 //        指定RecyclerView的布局方式，这里LinearLayoutManager是线性布局的意思，可以实现和ListView类似的效果
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+//        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
 //        设置布局的排列方式，默认是纵向的，下面设置成横向的
-        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+//        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+//        瀑布流布局
+//        StaggeredGridLayoutManager()的第一个参数是列数，第二个参数是排列方式
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(3, StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(layoutManager);
 //        创建FruitAdapter实例，并把水果的数据传递到FruitAdapter的构造函数当中
         FruitAdapter adapter = new FruitAdapter(fruitList);
