4a8e30fa770dea29f6f409ab38ef8e0e9271448d
pbw123 <2635225112@qq.com>
2019-09-04 18:00:22 +0800

    定制ListView界面

---
+++ ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.java
@@ -0,0 +1,46 @@
+package com.example.listviewtest;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+
+    private int resourceId;
+
+    public FruitAdapter(@NonNull Context context, int resource, @NonNull List<Fruit> objects) {
+        super(context, resource, objects);
+        resourceId=resource;
+    }
+
+    /**
+     * 每个子项滚动到屏幕内的时候getView()都会被调用
+     */
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        获取当前项的Fruit实例
+        Fruit fruit = getItem(position);
+//        加载我们传入的布局
+        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+//        获取ImageView实例
+        ImageView fruitImage = view.findViewById(R.id.fruit_image);
+//        获取TextView实例
+        TextView fruitName = view.findViewById(R.id.fruit_name);
+//        为ImageView设置要显示的图片
+        fruitImage.setImageResource(fruit.getImageId());
+//        为TextView设置要显示的名字
+        fruitName.setText(fruit.getName());
+//        将布局返回
+        return view;
+    }
+}
--- ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
+++ ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -3,19 +3,53 @@ package com.example.listviewtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
-private String[] data={"张三","李四","王五","赵六","陈浮生","陈富贵","竹叶青","陈龙象","陈半仙","王虎胜","张三千"};
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+    private String[] data = {"张三", "李四", "王五", "赵六", "陈浮生", "陈富贵", "竹叶青", "陈龙象", "陈半仙", "王虎胜", "张三千"};
+
+    private List<Fruit>fruitList=new ArrayList<>();
+
+    public void initFruits()
+    {
+        for (int i=0;i<2;i++)
+        {
+            Fruit zs = new Fruit("张三", R.drawable.apple_pic);
+            fruitList.add(zs);
+            Fruit ls = new Fruit("李四", R.drawable.banana_pic);
+            fruitList.add(ls);
+            Fruit ww = new Fruit("王五", R.drawable.orange_pic);
+            fruitList.add(ww);
+            Fruit cl = new Fruit("赵六", R.drawable.watermelon_pic);
+            fruitList.add(cl);
+            Fruit cfs = new Fruit("陈浮生", R.drawable.pear_pic);
+            fruitList.add(cfs);
+            Fruit clx = new Fruit("陈龙象", R.drawable.grape_pic);
+            fruitList.add(clx);
+            Fruit cbx = new Fruit("陈半仙", R.drawable.pineapple_pic);
+            fruitList.add(cbx);
+            Fruit zsq = new Fruit("张三千", R.drawable.strawberry_pic);
+            fruitList.add(zsq);
+            Fruit cys = new Fruit("陈圆殊", R.drawable.cherry_pic);
+            fruitList.add(cys);
+            Fruit whs = new Fruit("王虎胜", R.drawable.mango_pic);
+            fruitList.add(whs);
+
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayAdapter<String>adapter=new ArrayAdapter<String>(
-                MainActivity.this,android.R.layout.simple_list_item_1,data);
+        //        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+//                MainActivity.this, android.R.layout.simple_list_item_1, data);
+//        初始化水果数据
+        initFruits();
+        FruitAdapter adapter = new FruitAdapter(MainActivity.this,R.layout.fruit_item,fruitList);
+
         ListView listView = findViewById(R.id.list_view);
         listView.setAdapter(adapter);
     }
