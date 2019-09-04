f74b5e05d6b5e98ad289c8304cb41b1c591caf48
pbw123 <2635225112@qq.com>
2019-09-04 18:15:42 +0800

    提升ListView的运行效率

--- ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.java
+++ ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.java
@@ -1,6 +1,7 @@
 package com.example.listviewtest;
 
 import android.content.Context;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -31,16 +32,36 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
 //        获取当前项的Fruit实例
         Fruit fruit = getItem(position);
 //        加载我们传入的布局
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+        View view;
+        ViewHolder viewHolder;
+//        convertView是将我们加载好的布局进行缓存
+        if (convertView==null)
+        {
+             view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+             viewHolder=new ViewHolder();
+            viewHolder.imageView = view.findViewById(R.id.fruit_image);
+            viewHolder.textView = view.findViewById(R.id.fruit_name);
+            view.setTag(viewHolder);
+        }else {
+            view=convertView;
+             viewHolder =(ViewHolder) view.getTag();
+        }
+        viewHolder.imageView.setImageResource(fruit.getImageId());
+        viewHolder.textView.setText(fruit.getName());
 //        获取ImageView实例
-        ImageView fruitImage = view.findViewById(R.id.fruit_image);
-//        获取TextView实例
-        TextView fruitName = view.findViewById(R.id.fruit_name);
-//        为ImageView设置要显示的图片
-        fruitImage.setImageResource(fruit.getImageId());
-//        为TextView设置要显示的名字
-        fruitName.setText(fruit.getName());
+//        ImageView fruitImage = view.findViewById(R.id.fruit_image);
+////        获取TextView实例
+//        TextView fruitName = view.findViewById(R.id.fruit_name);
+////        为ImageView设置要显示的图片
+//        fruitImage.setImageResource(fruit.getImageId());
+////        为TextView设置要显示的名字
+//        fruitName.setText(fruit.getName());
 //        将布局返回
         return view;
+    }
+//    定义内部类
+    class  ViewHolder{
+        ImageView imageView;
+        TextView textView;
     }
 }
