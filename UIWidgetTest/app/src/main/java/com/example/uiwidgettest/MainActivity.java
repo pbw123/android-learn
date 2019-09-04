399e5a2cda82cb35100f10cf8c35eb15baa37e17
pbw123 <2635225112@qq.com>
2019-09-04 08:49:24 +0800

    Button的学习

--- UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.uiwidgettest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button:
+//                    业务逻辑
+                break;
+            default:
+                break;
+        }
     }
 }
