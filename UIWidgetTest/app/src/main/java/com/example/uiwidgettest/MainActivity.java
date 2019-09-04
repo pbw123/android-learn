b0d95fa535bbf267839c4acfbfd0af3542d3dddc
pbw123 <2635225112@qq.com>
2019-09-04 09:08:24 +0800

    ImageView的学习

--- UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -6,17 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText editText;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
-          editText = (EditText) findViewById(R.id.edit_text);
+        editText = (EditText) findViewById(R.id.edit_text);
+        imageView = (ImageView) findViewById(R.id.image_view);
         button.setOnClickListener(this);
     }
 
@@ -24,8 +28,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button:
-                String text = editText.getText().toString();
-                Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+//                String text = editText.getText().toString();
+//                Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
+                imageView.setImageResource(R.drawable.image_2);
                 break;
             default:
                 break;
