3bb8d9ca9e03b0c9c78bd2750592f7b6e80b96d0
pbw123 <2635225112@qq.com>
2019-09-04 09:00:06 +0800

    EditText的学习

--- UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -5,14 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button button = (Button) findViewById(R.id.button);
+          editText = (EditText) findViewById(R.id.edit_text);
         button.setOnClickListener(this);
     }
 
@@ -20,7 +24,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button:
-//                    业务逻辑
+                String text = editText.getText().toString();
+                Toast.makeText(MainActivity.this, text, Toast.LENGTH_SHORT).show();
                 break;
             default:
                 break;
