7ec0cb70b07d44b6f4814b1e709654fe0724a2aa
pbw123 <2635225112@qq.com>
2019-09-06 12:44:39 +0800

    动态注册监听网络变化2

--- chapter5/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ chapter5/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -6,6 +6,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -35,7 +38,17 @@ public class MainActivity extends AppCompatActivity {
 //        每当网络状态发生变化时，onReceive都会得到执行
         @Override
         public void onReceive(Context context, Intent intent) {
-         Toast.makeText(context,"network is change",Toast.LENGTH_SHORT).show();
+//         Toast.makeText(context,"network is change",Toast.LENGTH_SHORT).show();
+//            ConnectivityManager是一个系统服务类，专门用于管理网络连接
+            ConnectivityManager connectivityManager=
+                    (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo!=null&&networkInfo.isAvailable())
+            {
+                Toast.makeText(context,"有网络",Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(context,"没网络",Toast.LENGTH_SHORT).show();
+            }
         }
     }
 }
