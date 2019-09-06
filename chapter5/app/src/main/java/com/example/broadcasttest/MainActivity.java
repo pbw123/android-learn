3b966464d2a82f46d7d6c18736ce892568988194
pbw123 <2635225112@qq.com>
2019-09-06 16:18:06 +0800

    发送自定义广播--发送标准广播

--- chapter5/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ chapter5/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -10,6 +10,8 @@ import android.net.ConnectivityManager;
 import android.net.Network;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +27,15 @@ public class MainActivity extends AppCompatActivity {
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver= new NetworkChangeReceiver();
         registerReceiver(networkChangeReceiver, intentFilter);
+
+        Button button=(Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.example.broadcasttest.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
     }
 
     @Override
